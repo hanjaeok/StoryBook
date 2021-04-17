2b1524e6b04c55fa50428d91a9f847f0ef005f42
han-ker <hanjaeok17@gmail.com>
2021-04-17 15:46:28 +0900

    2021.04.17 Spring Security 설정

---
+++ src/main/java/com/hanker/storybook/StoryBookApplication.java
@@ -0,0 +1,13 @@
+package com.hanker.storybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoryBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanker/storybook/StoryBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanker.storybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
